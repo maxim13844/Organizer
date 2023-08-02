71be295c8201fbda3aafe94825e4c8e54b8b8a24
maxim13844 <maxim13844@gmail.com>
2023-08-02 09:24:17 +0300

    create model

--- countries/codes/models.py
+++ countries/codes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    code = models.CharField(max_length=2)
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f'Code: {self.code} <br> Country: <b>{self.name}</b>'
+    
--- countries/codes/views.py
+++ countries/codes/views.py
@@ -1,14 +1,26 @@
 from django.http import HttpResponse
 
-from codes.data import countries
+from codes.models import Country
+
+
+'''def get_country_name(request):
+    if 'code' not in request.GET:
+        return HttpResponse('code param is required')
+    code_input = request.GET['code'].upper()
+    country = Country.objects.filter(code=code_input).first()
+    if country:
+        country_name = country.name
+    else:
+        country_name = 'Country not found'
+    return HttpResponse(f'Code: {code_input} <br> Country: <b>{country_name}</b>')'''
 
 
 def get_country_name(request):
     if 'code' not in request.GET:
         return HttpResponse('code param is required')
     code_input = request.GET['code'].upper()
-    country = countries.get(code_input, 'Country not found')
-    return HttpResponse(f'Code: {code_input} <br> Country: <b>{country}</b>')
+    return HttpResponse(Country.objects.filter(code=code_input).first() or 'Country not found')
+
 
 
 
